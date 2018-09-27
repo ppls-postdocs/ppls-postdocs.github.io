3b99dfad0473723692120bd2c8596590f28c4939
Alexander <alxndr.martin@gmail.com>
2018-09-27 11:06:03 +0100

    Alex added people info

--- js/people.js
+++ js/people.js
@@ -1,9 +1,16 @@
 var allpeople= [
-	{	"name":"Dr. Christine Cuskley",
+	{	"name":"Dr Christine Cuskley",
 		"website":"https://ccuskley.github.io",
 		"twitter":"https://twitter.com/nerdpro",
 		"bio":'Funded by a <a href="https://britac.ac.uk" target="_blank">British Academy</a> Postdoctoral Fellowship through March 2019. Affiliated with the <a href="http://www.lel.ed.ac.uk/cle/" target="_blank">Centre for Language Evolution</a>.',
 		"subject":"Linguistics"
-	}
+	},
+    {"name":"Dr Alexander Martin",
+     "website":"https://lscp.net/persons/martin/",
+     "twitter":"https://twitter.com/jenaimarretin",
+     "github":"https://github.com/a-martin",
+     "bio":"I rock.  What more is there to say?",
+     "subject":"Being great"
 
+    }
 ]
