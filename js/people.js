539758003c4065fea3c98a935077b4ac8c416ec7
Alexander <alxndr.martin@gmail.com>
2018-09-27 14:06:23 +0100

    did some stuff and it was fine

--- js/people.js
+++ js/people.js
@@ -6,7 +6,7 @@ var allpeople= [
 		"subject":"Linguistics"
 	},
     {"name":"Dr Alexander Martin",
-     "website":"https://lscp.net/persons/martin/",
+     "website":"http://www.lscp.net/persons/martin/",
      "twitter":"https://twitter.com/jenaimarretin",
      "github":"https://github.com/a-martin",
      "bio":'Part of the ESRC project ‘Connecting cognitive biases and typological universals in syntax’. Affiliated with the <a href="http://www.lel.ed.ac.uk/cle/" target="_blank">Centre for Language Evolution</a>.',
