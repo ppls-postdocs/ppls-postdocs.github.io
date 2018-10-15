d6c10c953b005b75b4b2c72e7eb3357bed103d02
Alexander Martin <a-martin@users.noreply.github.com>
2018-10-15 11:40:57 +0100

    Update people.js
    added people and redid formatting

--- js/people.js
+++ js/people.js
@@ -1,30 +1,40 @@
 var allpeople= [
-	{	"name":"Dr Christine Cuskley",
-		"website":"https://ccuskley.github.io",
-		"twitter":"https://twitter.com/nerdpro",
-		"bio":'Funded by a <a href="https://britac.ac.uk" target="_blank">British Academy</a> Postdoctoral Fellowship through March 2019. Affiliated with the <a href="http://www.lel.ed.ac.uk/cle/" target="_blank">Centre for Language Evolution</a>.',
-		"subject":"Linguistics"
-	},
-    {"name":"Dr Alexander Martin",
-     "website":"http://www.lscp.net/persons/martin/",
-     "twitter":"https://twitter.com/jenaimarretin",
-     "github":"https://github.com/a-martin",
-     "bio":'Part of the ESRC project ‘Connecting cognitive biases and typological universals in syntax’. Affiliated with the <a href="http://www.lel.ed.ac.uk/cle/" target="_blank">Centre for Language Evolution</a>.',
-     "subject":"Linguistics"
-
+    {
+	"name":"Dr Christine Cuskley",
+	"website":"https://ccuskley.github.io",
+	"twitter":"https://twitter.com/nerdpro",
+	"bio":'Funded by a <a href="https://britac.ac.uk" target="_blank">British Academy</a> Postdoctoral Fellowship through March 2019. Affiliated with the <a href="http://www.lel.ed.ac.uk/cle/" target="_blank">Centre for Language Evolution</a>.',
+	"subject":"Linguistics"
     },
-{"name":"Dr Cristina Grisot",
-     "website":"https://www.unige.ch/lettres/linguistique/collaborateurs/postdocs/cristina-grisot/",
-     "twitter":"",
-     "github":"",
-     "bio":'Funded by an Advanced Postdoctoral grant from the Swiss SNSF, working with Dr. Hannah Rohde experimental investigations of human processing of implicit and overtly marked temporal relations.',
-     "subject":"Linguistics"
-
+    {
+	"name":"Dr Alexander Martin",
+	"website":"http://www.lscp.net/persons/martin/",
+	"twitter":"https://twitter.com/jenaimarretin",
+	"github":"https://github.com/a-martin",
+	"bio":'Part of the ESRC project ‘Connecting cognitive biases and typological universals in syntax’. Affiliated with the <a href="http://www.lel.ed.ac.uk/cle/" target="_blank">Centre for Language Evolution</a>.',
+	"subject":"Linguistics"
     },
-{"name":"Dr Raffaela Baechler",
- "website":"https://www.ed.ac.uk/profile/raffaela-baechler",
- "bio":'Funded by a postdoctoral grant (Forschungsstipendium) from the German DFG. Affiliated with the <a href="http://www.amc.lel.ed.ac.uk/" target="_blank">Angus McIntosh Centre for Historical Linguistics</a>.',
- "subject":"Linguistics",
- "twitter":"",
- "github":""
+    {
+	"name":"Dr Drew Altschul",
+	"website":"https://www.ed.ac.uk/profile/drewmaltschul",
+	"twitter":"https://twitter.com/dremalt",
+	"github":"https://github.com/dmaltschul",
+	"bio":'Postdoctoral fellow in cognitive epidemiology, and research associate with the MRC Data Pathfinder project: ‘Mental health within the family and between generations’. Affiliated with the <a href="https://www.ccace.ed.ac.uk/" target="_blank">Centre for Cognitive Ageing and Cognitive Epidemiology</a>. Studying cognition, personality, health, and hominoid evolution in large samples of humans and non-human primates.',
+     "subject":"Psychology"
+    },
+    {
+	"name":"Dr Raffaela Baechler",
+	"website":"https://www.ed.ac.uk/profile/raffaela-baechler",
+	"bio":'Funded by a postdoctoral grant (Forschungsstipendium) from the German DFG. Affiliated with the <a href="http://www.amc.lel.ed.ac.uk/" target="_blank">Angus McIntosh Centre for Historical Linguistics</a>.',
+	"subject":"Linguistics"
+    },
+    {
+	"name":"Dr Cristina Grisot",
+	"website":"https://www.unige.ch/lettres/linguistique/collaborateurs/postdocs/cristina-grisot/",
+	"twitter":"",
+	"github":"",
+	"bio":'Funded by an Advanced Postdoctoral grant from the Swiss SNSF, working with Dr. Hannah Rohde experimental investigations of human processing of implicit and overtly marked temporal relations.',
+	"subject":"Linguistics"
+    },
 ]
+
