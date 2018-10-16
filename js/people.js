9c3441996c61402ac7ae973bf005437a7ef5ad20
Christine Cuskley <ccuskley@gmail.com>
2018-10-16 13:41:53 +0100

    c github

--- js/people.js
+++ js/people.js
@@ -3,6 +3,7 @@ var allpeople= [
 	"name":"Dr Christine Cuskley",
 	"website":"https://ccuskley.github.io",
 	"twitter":"https://twitter.com/nerdpro",
+	"github":"https://github.com/ccuskley",
 	"bio":'Funded by a <a href="https://britac.ac.uk" target="_blank">British Academy</a> Postdoctoral Fellowship through March 2019. Affiliated with the <a href="http://www.lel.ed.ac.uk/cle/" target="_blank">Centre for Language Evolution</a>.',
 	"subject":"Linguistics"
     },
